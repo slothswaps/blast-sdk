38cb2464925148a1bdf1c34b86d92b32907a46da
slothswap <158803507+slothswaps@users.noreply.github.com>
2024-02-20 15:09:11 +0800

    Add files via upload

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'HMDX',
-      'HSwap LP Token'
+      'LP',
+      'LP Token'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
